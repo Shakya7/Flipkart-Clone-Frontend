33245e0f3e1b35c6fdd4630a031c46d228883e3b
Shakya7 <shakyasarkar72@gmail.com>
2023-02-01 16:48:45 +0530

    OrderPage made responsive

--- src/components/OrderBlocks.js
+++ src/components/OrderBlocks.js
@@ -5,7 +5,7 @@ function OrderBlocks(props){
 
     const {state,dispatch}=useContext(GlobalContext);
     return(
-        <div className="transition-transform duration-500 ease-in-out cursor-pointer hover:scale-105 w-full h-auto bg-white flex justify-around items-center p-3 shadow-lg shadow-gray-500 rounded-sm">
+        <div className="transition-transform duration-500 ease-in-out cursor-pointer hover:scale-105 w-full h-auto bg-white flex flex-col xlsm:flex-row justify-around items-center p-3 shadow-lg shadow-gray-500 rounded-sm">
                 <div className="w-[10vmax] h-auto">
                 {
                     props.item.orders.length?
@@ -13,23 +13,23 @@ function OrderBlocks(props){
                     {
                         props.item.orders.map((el,i)=>{
                             return(
-                            <div className="flex items-center gap-2 mt-2.5" key={i}>
+                            <div className="flex items-center gap-2 mt-2.5 flex-col lg:flex-row text-z xxxxsm:text-base" key={i}>
                                 <img className="w-14" src={el.image}/>
-                                <p>Qty: {el.quantity}</p>
+                                <p>Qty: <span className="font-bold">{el.quantity}</span></p>
                             </div>)
                         })
                     }    
                     </div>:""
                 }
                 </div>
-                <div className="flex flex-col gap-5">
+                <div className="flex flex-col gap-5 text-z xxxxsm:text-base">
                     <p><b>Delivered to:</b> {props.item.address}</p>  
                     <div>
                         <p><b>Payment done on:</b></p>
                         <div className="bg-green-600 p-2 rounded-full text-white text-center">{props.item.paymentDate}</div>
                     </div>  
                 </div> 
-                <div className="justify-self-end">
+                <div className="justify-self-end text-z xxxxsm:text-base">
                     <b>₹{props.item.price}</b>
                 </div>
         </div>
--- src/components/OrdersPage.js
+++ src/components/OrdersPage.js
@@ -47,15 +47,15 @@ function OrdersPage(){
         <div>
             <Categories/>
             <div className="w-full min-h-auto bg-white flex gap-2 items-start justify-evenly py-5">
-                <div className="w-1/5 bg-blue-900 h-52 text-white rounded-md relative shadow-lg shadow-gray-500">
+                <div className="hidden lg:block w-1/5 bg-blue-900 h-52 text-white rounded-md relative shadow-lg shadow-gray-500">
                     <div className="flex justify-center items-center flex-col mt-[5%] gap-5">
                         <p>Filter effects</p>
                         <div className="flex flex-col gap-4">
                             <div className="flex gap-3">
                                 <label>Price: ₹</label>
-                                <input className="text-center w-[5vw] text-black" value={priceFilter} onChange={e=>setPriceFilter(e.target.value)} type="number"/>
+                                <input className="text-center w-32 text-black w-min" value={priceFilter} onChange={e=>setPriceFilter(e.target.value)} type="number"/>
                             </div>
-                            <div className="flex gap-3">
+                            <div className="flex gap-3 flex-col xlg:flex-row">
                                 <input min="0" value={priceFilter} onChange={(e)=>{
                                     setPriceFilter(e.target.value);
                                     //loadData().then(res=>dispatch({type:"search-order-by-price",payload:res,price:priceFilter}))//
@@ -76,15 +76,42 @@ function OrdersPage(){
                         document.querySelector(".srch").value="";
                     }} className="absolute bottom-5 right-5 text-sm w-auto bg-red-500 px-2.5 py-1 rounded-md shadow-lg shadow-blue-500 cursor-pointer transition-colors ease-in-out duration-500 hover:bg-white hover:text-black">Clear</div>
                 </div>
-                <div className="w-2/3 bg-white flex flex-col h-auto gap-5">
-                    <div className="flex">
-                        <input className="srch w-4/5 h-[3vmax] rounded-l-md pl-[2%] border border-gray-400" onChange={e=>setSearchInp(e.target.value)} type="text" placeholder="Search orders by destination address keywords..."/>
-                        <div className="bg-blue-500 flex justify-evenly items-center p-1.5 text-white font-bold text-sm w-1/5 gap-5 rounded-r-md cursor-pointer">
-                            <img className="w-5" src={search_icon}/>
+                <div className="w-11/12 lg:w-2/3 bg-white flex flex-col h-auto gap-5">
+                    <div className="flex text-extraSmall md:text-base">
+                        <input className="srch w-4/5 rounded-l-md pl-[2%] border  border-gray-400" onChange={e=>setSearchInp(e.target.value)} type="text" placeholder="Search orders by destination address keywords..."/>
+                        <div className="bg-blue-500 flex justify-evenly items-center p-1.5 text-white font-bold text-sm w-1/5 gap-0 md:gap-5 rounded-r-md cursor-pointer text-extraSmall md:text-base">
+                            <img className="hidden md:block w-5" src={search_icon}/>
                             <p onClick={async()=>{
                                 let data=await loadData();
                                 dispatch({type:"search-order-by-address",payload:data,term:searchInp});
-                            }}>Search orders</p>
+                            }}>Search</p>
+                        </div>
+                    </div>
+                    <div className="block text-extraSmall md:text-base lg:hidden w-full">
+                        <b>Filters:</b>
+                        <div className="flex px-2 flex-col xxxxsm:flex-row xxxxsm:flex-wrap border border-gray-500 rounded-md items-center gap-3 py-3 border-dashed">
+                            <div className="flex justify-center gap-1">
+                                <p>Price: ₹</p>
+                                <input value={priceFilter} onChange={e=>setPriceFilter(e.target.value)} className="bg-gray-300 rounded-sm px-1.5" placeholder="Type in your filtered amount..." type="number"/>
+                            </div>
+                            <input min="0" className="w-10/12 xxxxsm:w-auto" value={priceFilter} onChange={(e)=>{
+                                setPriceFilter(e.target.value);
+                            }} step="1" max="10000" type={"range"}/>
+                            <div className="flex gap-3">
+                                <button className="cursor-pointer bg-black px-1.5 text-white rounded-md" onClick={async e=>{
+                                        let data=await loadData();
+                                        dispatch({type:"search-order-by-price",payload:data,price:priceFilter})
+                                }}>Search</button>
+                                <div onClick={async e=>{
+                                    let data=await loadData();
+                                    window.location.reload();
+                                    await dispatch({type:"load-user-data",payload:data});
+                                    setSearchInp("");
+                                    document.querySelector(".srch").value="";
+                                    }} className="w-auto bg-red-500 px-1.5 rounded-md shadow-lg hover:shadow-gray-500 cursor-pointer transition-colors ease-in-out duration-500 text-white hover:bg-white hover:text-black">
+                                        CLEAR
+                                </div>
+                            </div>
                         </div>
                     </div>
                     {
